edc2e1340083ba14a6eea6807f30b3e63e27a611
Hashmics <rehmanhashmirehman123@gmail.com>
2024-10-19 00:48:46 +0500

    Create user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv, { config } from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to mongoDB');
+}).catch((err) => {
+    console.log(err)
+});
 
 const app = express()
 
