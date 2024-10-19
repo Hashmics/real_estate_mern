24651ae97aea55f19c1ed899dcd4cb1800f2d071
Hashmics <rehmanhashmirehman123@gmail.com>
2024-10-20 01:29:09 +0500

    Complete Sign Up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
-import SignOut from './pages/SignOut'
+import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
@@ -9,11 +9,11 @@ import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
-        <Route path='/sign-out' element={<SignOut />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About />} />
         <Route path='/profile' element={<Profile />} />
       </Routes>
