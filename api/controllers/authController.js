63a803a54f1cc1e38c6ede0b85a5c1889b33ecbb
Hashmics <rehmanhashmirehman123@gmail.com>
2024-10-19 17:45:41 +0500

    create sign up api route

---
+++ api/controllers/authController.js
@@ -0,0 +1,25 @@
+import User from './../models/userModel.js';
+import bcryptjs from 'bcryptjs';
+
+export const signUp = async (req, res) => {
+
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10)
+
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    try {
+        await newUser.save();
+        res.status(201).json({
+            success: true,
+            message: 'User Created successfully'
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message: 'Failed to create user'
+        })
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv, { config } from 'dotenv'
 import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js';
 dotenv.config()
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
@@ -12,8 +13,11 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 const app = express()
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is Running on port 3000')
 });
 
-app.use("/api/user", userRouter)
+app.use("/api/user", userRouter);
+app.use('/api/auth', authRouter);
